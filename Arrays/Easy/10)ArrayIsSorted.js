81e88e9437a1381d3bb0e2bb2dea8563f43a6b28
Akshaay9 <nair.akshay9898@gmail.com>
2023-03-19 18:53:56 +0530

    array is sorted

--- Arrays/Easy/10)ArrayIsSorted.js
+++ Arrays/Easy/10)ArrayIsSorted.js
@@ -1,7 +1,7 @@
 const checkArrayIsSorted = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] >= arr[i - 1]) {
-      //
+      // dont do anything
     } else {
       return false;
     }

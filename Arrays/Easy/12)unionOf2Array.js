fdb9fe529c2a07af5f289845df3fdc7696d9458c
Akshaay9 <nair.akshay9898@gmail.com>
2023-03-20 19:30:42 +0530

    unionOf2 Array

---
+++ Arrays/Easy/12)unionOf2Array.js
@@ -0,0 +1,44 @@
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const arr2 = [2, 3, 4, 4, 5, 11, 12];
+
+const unionOfArr = (arr1, arr2) => {
+  const result = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    const currI = arr1[i];
+    const currJ = arr2[j];
+    const lastEle = result[result.length - 1];
+    if (currI < currJ) {
+      if (result.length == 0 || lastEle !== currI) {
+        result.push(currI);
+      }
+      i += 1;
+    } else {
+      if (result.length === 0 || lastEle !== currJ) {
+        result.push(currJ);
+      }
+      j += 1;
+    }
+  }
+
+  while (i < arr1.length) {
+    if (arr1[i] !== result[result.length - 1]) {
+      result.push(arr1[i]);
+    }
+    i++;
+  }
+  while (j < arr2.length) {
+    if (arr2[j] !== result[result.length - 1]) {
+      result.push(arr2[j]);
+    }
+    j++;
+  }
+
+  return result;
+};
+
+console.log(unionOfArr(arr1, arr2));

9df117f9d1e285456998b7fdf33b11e6a6f89225
Akshay Nair <72427030+Akshaay9@users.noreply.github.com>
2023-10-27 13:00:27 +0530

    fuck my life

--- Arrays/Easy/7)ForwardJumps.js
+++ Arrays/Easy/7)ForwardJumps.js
@@ -1,12 +1,15 @@
-const forwardRotateArr = (arr, steps) => {
-    let result = [...arr];
-  
-    for (let i = 0; i < arr.length; i++) {
-      let stepsToBeTaken = (i + steps) % arr.length;
-      arr[stepsToBeTaken] = result[i];
-    }
-    return arr;
-  };
-  
-  console.log(forwardRotateArr([1, 2, 3, 4, 5, 6, 7], 3));
-  
+const rotateArr = (array, rotate) => {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let jumps = (i + rotate)%array.length;
+    result[jumps] = arr[i];
+  }
+  return result;
+};
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const length = 7;
+const rotate = 2;
+
+console.log(rotateArr(arr, rotate));
--- Arrays/Easy/8)reverseRotateArr.js
+++ Arrays/Easy/8)reverseRotateArr.js
@@ -1,14 +1,18 @@
-const reverseRotateArr = (arr, steps) => {
-  let result = [...arr];
+const rotateArr = (array, rotate) => {
+  const result = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    let stepsToBeTaken = i - steps;
-    if (stepsToBeTaken < 0) {
-      stepsToBeTaken = arr.length + stepsToBeTaken;
+  for (let i = 0; i < array.length; i++) {
+    let jumps = i - rotate;
+    if (jumps < 0) {
+      jumps = arr.length + jumps;
     }
-    arr[stepsToBeTaken] = result[i];
+    result[jumps] = arr[i];
   }
-  return arr;
+  return result;
 };
 
-console.log(reverseRotateArr([1, 2, 3, 4, 5, 6, 7], 3));
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const length = 7;
+const rotate = 2;
+
+console.log(rotateArr(arr, rotate));
